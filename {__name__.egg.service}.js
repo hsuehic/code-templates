f082990244e54a2c49cee1ee7e8f3b3ab5d77c03
xiaowei.xue <xiaowei.xue@shopee.com>
2018-11-14 10:55:18 +0800

    feat: add egg.service template.

---
+++ {__name__.egg.service}.js
@@ -0,0 +1,20 @@
+/**
+ * {__name__}.jsx
+ * @author: {__author__}({__email__})
+ * @link: {__link__}
+ * @date: {__date__}
+ */
+
+'use strict';
+
+const Service = require('egg').Service;
+
+class { __pascalCaseName__ } Service extends Service {
+  async getList() {
+    const { ctx } = this;
+
+    return ctx.request.body;
+  }
+}
+
+module.exports = { __pascalCaseName__ }Service;
